4243d9b7adc84d229dab665d3a60ff92abcb2cf5
dankloop <hidasior@gmail.com>
2022-01-02 20:26:08 -0500

    api key hidden

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,6 +1,7 @@
 import { Company } from './Company';
 import { User } from './User';
 import { Loader } from '@googlemaps/js-api-loader';
+import { GOOGLE_MAPS_API_KEY } from '../config.js';
 
 const user = new User();
 console.log(user);
@@ -9,7 +10,7 @@ const company = new Company();
 console.log(company);
 
 const loader = new Loader({
-  apiKey: 'AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU',
+  apiKey: GOOGLE_MAPS_API_KEY,
   version: 'weekly',
 });
 
